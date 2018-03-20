0afd532403e05e6f714fb60236423ccef5cc5bad
NotAPenguin <michiel.vandeginste@gmail.com>
2018-03-20 21:54:55 +0100

    added template Deleter parameter
    Deleter parameter defaults to
    decltype(dont_delete<TO>)

--- GL_Utility/mem_util.h
+++ GL_Utility/mem_util.h
@@ -7,8 +7,8 @@ namespace mem
 
 	/*Convert a shared_ptr<void> to shared_ptr<T>
 	 *Template parameter U represents the type the resulting smart pointer will hold*/
-	template<typename TO>
-	std::shared_ptr<TO> cast_from_void(std::shared_ptr<void> original, void (*deleter)(TO*) = dont_delete<TO>)
+	template<typename TO, typename Deleter = decltype(dont_delete<TO>)>
+	std::shared_ptr<TO> cast_from_void(std::shared_ptr<void> original, Deleter deleter = dont_delete<TO>)
 	{
 		auto raw_ptr = original.get();
 		auto converted_ptr = (TO*)raw_ptr;
