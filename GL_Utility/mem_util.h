4132ce32e98df3dcb9273731c7dec6bfcbb47675
NotAPenguin <michiel.vandeginste@gmail.com>
2018-03-21 18:59:09 +0100

    added default_deleter
    added default_deleter to mem_util.h and changed default deleter used by cast_from_void to this deleter

--- GL_Utility/mem_util.h
+++ GL_Utility/mem_util.h
@@ -5,10 +5,12 @@ namespace mem
 	/*Custom deleter that does nothing*/
 	template<typename U> void dont_delete(U*) {  }
 
+	template<typename U> void default_deleter(U* obj) { delete obj; }
+
 	/*Convert a shared_ptr<void> to shared_ptr<T>
 	 *Template parameter U represents the type the resulting smart pointer will hold*/
-	template<typename TO, typename Deleter = decltype(dont_delete<TO>)>
-	std::shared_ptr<TO> cast_from_void(std::shared_ptr<void> original, Deleter deleter = dont_delete<TO>)
+	template<typename TO, typename Deleter = decltype(default_deleter<TO>)>
+	std::shared_ptr<TO> cast_from_void(std::shared_ptr<void> original, Deleter deleter = default_deleter<TO>)
 	{
 		auto raw_ptr = original.get();
 		auto converted_ptr = (TO*)raw_ptr;
